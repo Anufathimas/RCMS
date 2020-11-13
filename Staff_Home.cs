1266ac32726cc1db74fbcae50688dfac8656a647
jufiG <69841568+jufiG@users.noreply.github.com>
2020-11-13 20:27:11 +0700

    Add files via upload

---
+++ Staff_Home.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RCMS
+{
+    public partial class Staff_Home : Form
+    {
+        public Staff_Home()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            Staff_CampMembers obj = new Staff_CampMembers();
+            obj.Show();
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            Staff_AddStock obj = new Staff_AddStock();
+            obj.Show();
+        }
+
+        private void toolStripButton7_Click(object sender, EventArgs e)
+        {
+            Staff_OrderItems obj = new Staff_OrderItems();
+            obj.Show();
+
+        }
+
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
