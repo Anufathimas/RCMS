e34b84a90300bc06c57dfa4e8a5b9fb4cb043d05
Anufathimas <71185004+Anufathimas@users.noreply.github.com>
2020-11-21 21:55:52 +0530

    Add files via upload

---
+++ Collector_CollectorateStaff.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace RCMS
+{
+    public partial class Collector_CollectorateStaff : Form
+    {
+        BaseConnection con = new BaseConnection();
+        public Collector_CollectorateStaff()
+        {
+            InitializeComponent();
+            fillgrid();
+        }
+
+        public void fillgrid()
+        {
+            try
+            {
+                string query = "select * from CollectorateStaff_Details";
+                DataSet ds = con.ret_ds(query);
+                dataGridView1.DataSource = ds.Tables[0].DefaultView;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("exception occured....");
+            }
+        }
+
+        private void toolStripButton7_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
