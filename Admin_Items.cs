1dd48f31d0164ff19a323f595ef5b951d673690e
sree-27 <71020044+sree-27@users.noreply.github.com>
2020-11-24 18:26:24 +0530

    Add files via upload

---
+++ Admin_Items.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace RCMS
+{
+    public partial class Admin_Items : Form
+    {
+        BaseConnection con = new BaseConnection();
+        public static string cid = "";
+        public Admin_Items()
+        {
+            InitializeComponent();
+            fillcid();
+            fillgrid();
+
+        }
+        public void fillcid()
+        {
+            try
+            {
+                string query = "select isnull(max(catid),100)+1 from Category_Details";
+                SqlDataReader dr = con.ret_dr(query);
+                if (dr.Read())
+                {
+                    cid = dr[0].ToString();
+                    ID.Text = dr[0].ToString();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("exception occured....");
+            }
+        }
+
+        public void cleartab1()
+        {
+            ID.Text = "";
+            name.Text = "";
+            items.Text = "";
+            Units.Text = "";
+
+        }
+
+        public void fillgrid()
+        {
+            try
+            {
+                string query = "select * from Category_Details order by catid asc";
+                DataSet ds = con.ret_ds(query);
+                dataGridView1.DataSource = ds.Tables[0].DefaultView;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("exception occured....");
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+           try
+           {
+               string query = "insert into Category_Details values(" + cid + ",'"+name.Text+"','"+items.Text+"','"+Units.Text+"')";
+            if (con.exec1(query) > 0)
+            {
+                MessageBox.Show("Category details added......");
+
+                cleartab1();
+                fillgrid();
+                fillcid();
+            }
+            else
+            {
+                MessageBox.Show("Problem encountered while adding Category details.....");
+                cleartab1();
+                fillgrid();
+            }
+        }
+            catch (Exception ex)
+            {
+                MessageBox.Show("exception occured....");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            cleartab1();
+        }
+
+        private void toolStripButton7_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+
+    }
+}
