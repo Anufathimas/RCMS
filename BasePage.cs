37a7595c510f708305c9215e083ad65e6ed92508
Anufathimas <71185004+Anufathimas@users.noreply.github.com>
2020-12-09 12:32:49 +0530

    Add files via upload

---
+++ BasePage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RCMS
+{
+    public partial class BasePage : Form
+    {
+        public BasePage()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            Login obj = new Login();
+            obj.Show();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
