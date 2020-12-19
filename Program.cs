98a16f90f87083293f2565c92d427da8051e9ba6
Anufathimas <71185004+Anufathimas@users.noreply.github.com>
2020-12-19 22:10:23 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace RCMS
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        /// 
+
+        public static string uid = "";
+        public static string utype = "";
+
+        public static string district = "";
+        public static string csid = "";
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new BasePage());
+        }
+    }
+}
